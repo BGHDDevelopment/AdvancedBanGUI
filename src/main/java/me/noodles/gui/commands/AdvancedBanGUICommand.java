e9bda7ee874d3145299d836fcd78d7908bd02838
StephanBH <bghdpayments@gmail.com>
2019-07-11 13:49:38 -0700

    Added Kick

---
+++ src/main/java/me/noodles/gui/commands/AdvancedBanGUICommand.java
@@ -0,0 +1,34 @@
+package me.noodles.gui.commands;
+
+import me.noodles.gui.main.Main;
+import me.noodles.gui.utils.Settings;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class AdvancedBanGUICommand implements Listener, CommandExecutor {
+
+
+	public boolean onCommand(final CommandSender sender, final Command cmd, final String commandLabel, final String[] args) {
+		if (!(sender instanceof Player)) {
+			Bukkit.getServer().getLogger().warning(Main.plugin.getConfig().getString("NoPlayer"));
+			return true;
+		}
+		final Player p = (Player) sender;
+		if (!cmd.getName().equalsIgnoreCase("advancecbangui")) {
+			return true;
+		}
+		p.sendMessage(ChatColor.GRAY +  "****************************************************");
+		p.sendMessage(ChatColor.GOLD +  "Created by: " + ChatColor.WHITE + "" + Settings.DEVELOPER_NAME);
+		p.sendMessage(ChatColor.GOLD +  "Website: " + ChatColor.WHITE + "" + Settings.DEVELOPER_URL);
+		p.sendMessage(ChatColor.GOLD +  "Spigot Link: " + ChatColor.WHITE + "" + Settings.PLUGIN_URL);
+		p.sendMessage(ChatColor.GOLD +  "Support Link: " + ChatColor.WHITE + "" + Settings.SUPPORT_DISCORD_URL);
+		p.sendMessage(ChatColor.GRAY +  "****************************************************");
+		return true;
+	}
+
+}
---
+++ src/main/java/me/noodles/gui/utils/Settings.java
@@ -0,0 +1,11 @@
+package me.noodles.gui.utils;
+
+public class Settings {
+
+    public static String DEVELOPER_NAME = "BGHDDevelopment";
+    public static String DEVELOPER_URL = "https://bghddevelopment.com";
+    public static String PLUGIN_URL = "https://spigotmc.org/resources/59570";
+    public static String SUPPORT_DISCORD_URL = "https://bghddevelopment.com/discord";
+    public static String VERSION = "2.0";
+    public static String NAME = "AdvancedBanGUI";
+}
